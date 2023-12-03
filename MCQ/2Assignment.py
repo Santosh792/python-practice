3394cb1d02e623a10c2aae5202b58fa6f62c5f2f
Santosh792 <santosh792kumar@gmail.com>
2023-12-03 21:35:11 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ MCQ/2Assignment.py
@@ -0,0 +1,40 @@
+#Assignment2 MCQ
+#Input1:
+'''Tech="Python"
+print(Tech*len(Tech))'''
+
+#Input2:
+'''Name=["santosh",'kumar','yadav','Babu']
+print(Name[-2][2])'''
+
+#Input3:
+'''Company = 'Infosys' + \
+           '\nTechnologies' + \
+           '\nLimited'
+print('Company')'''
+
+def add(num1=2, num2=2):
+    return (num1+num2)
+print(add(4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Output1: TechTechTechTechTechTech
+#Output2:d (-2 index refer from last which is 'yadav', 2nd indev is 'yadav' itself but start from front and zero)
+#Output3: Company
